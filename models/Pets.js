a39ecb61c676b737e8a4c29cfe6110e1b38b9b25
LilTzar <eeoerkeedu@gmail.com>
2022-03-24 10:35:22 -0600

    setting better views

--- models/Pets.js
+++ models/Pets.js
@@ -4,46 +4,46 @@ const sequelize = require("../config/connection");
 class Pets extends Model {}
 
 Pets.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    photo: {
-      type: DataTypes.STRING,
-    },
-    species: {
-      type: DataTypes.STRING,
-    },
-    type: {
-      type: DataTypes.STRING,
-    },
-    breed: {
-      type: DataTypes.STRING,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    gender: {
-      type: DataTypes.STRING,
-    },
-    age: {
-      type: DataTypes.INTEGER,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-    contact: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "pets",
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		photo: {
+			type: DataTypes.STRING,
+		},
+		species: {
+			type: DataTypes.STRING,
+		},
+		type: {
+			type: DataTypes.STRING,
+		},
+		breed: {
+			type: DataTypes.STRING,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+		gender: {
+			type: DataTypes.STRING,
+		},
+		age: {
+			type: DataTypes.INTEGER,
+		},
+		description: {
+			type: DataTypes.STRING,
+		},
+		contact: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "pets",
+	}
 );
 module.exports = Pets;
