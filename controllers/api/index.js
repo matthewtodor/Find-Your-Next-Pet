b7f3a469efb74c6f72f910b05decd91217401f0b
pdavisDU <pdavisdu@gmail.com>
2022-03-24 19:28:39 -0600

    changed login signup cards

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const petRoutes = require('./petRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/pets', petRoutes);
 router.use('/user', userRoutes);
+router.use('/pets', petRoutes);
 
 module.exports = router;
