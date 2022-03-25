994f37f55a5fabceb02d6927ce7b0d3b848d3f11
LilTzar <eeoerkeedu@gmail.com>
2022-03-25 10:05:42 -0600

    corrections to partials and view for HB

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const key = "sW47nEJ6VGOP4yi0DTBFrQ5M82BOoXMmoA8Z5Jqtx9rhhqoeRH";
 const secret = "OEPgGe511xyFpGLdy8Kzz6902FJX3brHxu5uGbec";
 
 var pf = new petfinder.Client({
-  apiKey: process.env.API_KEY,
-  secret: process.env.SECRET,
+	apiKey: process.env.API_KEY,
+	secret: process.env.SECRET,
 });
 
 pf.animal
-  .search({
-    type: "cat",
-    // breed: searchBreed,
-    // page,
-    limit: 100,
-  })
-  .then(function (response) {
-    console.log(response.data);
-    // Do something with `response.data.animals`
-  })
-  .catch(function (error) {
-    console.log(error);
-    // Handle the error
-  });
+	.search({
+		type: "cat",
+		// breed: searchBreed,
+		// page,
+		limit: 100,
+	})
+	.then(function (response) {
+		console.log(response.data);
+		// Do something with `response.data.animals`
+	})
+	.catch(function (error) {
+		console.log(error);
+		// Handle the error
+	});
