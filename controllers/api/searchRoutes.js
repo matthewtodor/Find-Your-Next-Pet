c6fa5087797e1d1f15766dc800529ee523f93e82
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-03-25 12:00:15 -0600

    updated routes, they now load pages!"

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -1,28 +1,36 @@
 const router = require("express").Router();
-const { SearchedPets } = require("../../models");
+const { SearchedPets } = require("../../models/Search");
 
 router.get("/", async (req, res) => {
-  let sessionIndex = req.session.pets;
-  console.log("this is on the search route", req.session.pets[0]);
   try {
-    const newSearch = await SearchedPets.create({
-      type: sessionIndex.type,
-      breeds: sessionIndex.breeds[0],
-      age: sessionIndex.age,
-      gender: sessionIndex.gender,
-      size: sessionIndex.size,
-      name: sessionIndex.name,
-      description: sessionIndex.description,
-      photo: sessionIndex.photos[3],
-      status: sessionIndex.status,
-      published_at: sessionIndex.published_at,
-      contact: sessionIndex.contact,
-    });
-    res.json(newSearch);
     res.render("searchpage");
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
+
+// router.post("/", async (req, res) => {
+//   let sessionIndex = req.session.pets;
+//   // console.log("this is on the search route", req.session.pets[0]);
+//   try {
+//     const newSearch = await SearchedPets.create({
+//       type: sessionIndex.type,
+//       breeds: sessionIndex.breeds[0],
+//       age: sessionIndex.age,
+//       gender: sessionIndex.gender,
+//       size: sessionIndex.size,
+//       name: sessionIndex.name,
+//       description: sessionIndex.description,
+//       photo: sessionIndex.photos[3],
+//       status: sessionIndex.status,
+//       published_at: sessionIndex.published_at,
+//       contact: sessionIndex.contact,
+//     });
+//     res.json(newSearch);
+//     res.render("searchpage");
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 module.exports = router;
