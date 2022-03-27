e03f90f3d213bedb297a3deb03e378837b8690e1
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-03-27 12:06:02 -0600

    round of saving

--- public/js/search.js
+++ public/js/search.js
@@ -32,6 +32,7 @@ const searchButtonHandler = async function (event) {
       //   console.log(result);
       // });
       console.log(data);
+      console.log(data.body);
       console.log(data.json());
       // function searchData(data) {
       //   return data.json()
@@ -40,7 +41,28 @@ const searchButtonHandler = async function (event) {
       // console.log(result);
 
       searchEl = document.getElementById("searchresults");
-      searchEl.innerHTML = "";
+      const searchCard = `<div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <div class="savedpetimageholder">
+            <img src="{{petSearchCall.[0].primary_photo_cropped.full}}" href="/pets/:id" class="img-fluid" alt="{{description}}">
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <i class="savedhearticon"><button id="{{id}}">Save</button></i>
+            <h5 class="card-title">{{name}}</h5>
+            <ul class="card-text" style="list-style: none;">
+              <li>{{gender}}</li>
+              <li>{{breed.primary}}</li>
+              <li>{{age}}</li>
+            </ul>
+            <p class="card-text"><small class="text-muted">{{contact.address.postcode}}</small></p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+      searchEl.innerHTML = ``;
 
       //   searchEl.innerHTML = `<div class="card mb-3">
       //   <div class="row g-0">
