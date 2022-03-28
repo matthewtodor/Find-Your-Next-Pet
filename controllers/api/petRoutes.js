19fe2c45596ec08d17f28155c38836a9f00d8f4f
Matthew Todor <unclejunglegeorge@gmail.com>
2022-03-28 14:28:29 -0600

    fixed pathing on authentiction

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -1,36 +1,36 @@
 const router = require("express").Router();
 const req = require("express/lib/request");
 const { Pets, User } = require("../../models");
-const withAuth = require("..utils/auth");
+const withAuth = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
-  try {
-    const petDB = await User.findAll({
-      include: [{ model: Pets, through: user_saved }],
-    });
-    const pet = petDB.map((data) => data.get({ plain: true }));
+	try {
+		const petDB = await User.findAll({
+			include: [{ model: Pets, through: user_saved }],
+		});
+		const pet = petDB.map((data) => data.get({ plain: true }));
 
-    // res.json(petDB);
-    res.render("savedpets", { pet });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		// res.json(petDB);
+		res.render("savedpets", { pet });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 // Get a pet that matches the id of the requested pet
 router.get("/:id", async (req, res) => {
-  try {
-    // finds the pet by the primary key, which is id
-    const petDetails = await Pets.findByPk(req.params.id);
-    // if it 404s, return the message below
-    if (!petDetails) {
-      res.status(404).json({ message: "No pet found with that id!" });
-      return;
-    }
-    res.status(200).json(petDetails);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		// finds the pet by the primary key, which is id
+		const petDetails = await Pets.findByPk(req.params.id);
+		// if it 404s, return the message below
+		if (!petDetails) {
+			res.status(404).json({ message: "No pet found with that id!" });
+			return;
+		}
+		res.status(200).json(petDetails);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 // creates a row for the saved pet with the included columns
 router.post("/", withAuth, async (req, res) => {
@@ -74,21 +74,21 @@ router.post("/", withAuth, async (req, res) => {
 });
 // removes a pet from the saved pets list
 router.delete("/", async (req, res) => {
-  try {
-    const savedPetCard = Pets.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
+	try {
+		const savedPetCard = Pets.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
 
-    if (!savedPetCard) {
-      res.status(404).json({ message: "No pet found with that id!" });
-      return;
-    }
-    res.status(200).json(savedPetCard);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		if (!savedPetCard) {
+			res.status(404).json({ message: "No pet found with that id!" });
+			return;
+		}
+		res.status(200).json(savedPetCard);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,24 +6,24 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-//const helpers = require("./utils/helpers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
+	secret: "Super secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening on http://localhost:3001"));
+	app.listen(PORT, () => console.log("Now listening on http://localhost:3001"));
 });
