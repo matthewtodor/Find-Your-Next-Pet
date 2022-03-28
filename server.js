f8eba3abff4b6434ba1997df1e8b8719f9cc3006
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-03-28 17:02:13 -0600

    fixing deployment css

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-	secret: "Super secret secret",
-	cookie: {},
-	resave: false,
-	saveUninitialized: true,
-	store: new SequelizeStore({
-		db: sequelize,
-	}),
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -30,10 +30,10 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log("Now listening on http://localhost:3001"));
+  app.listen(PORT, () => console.log("Now listening on http://localhost:3001"));
 });
